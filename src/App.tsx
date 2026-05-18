64f3d8f0ed13ebc36634dab0ba443bbb5ff060b3
Carlos <carlo@kinomori.ma>
2026-05-18 17:04:21 +0200

    fix(routing): usar HashRouter para que las rutas #/story funcionen en estáticos
    - Antes: <Routes> sin Router → todas las rutas caían a /
    - Ahora: <HashRouter> con hash URLs (/#/story, /#/menu, etc.)
    - Compatible con Netlify, GitHub Pages, cualquier host estático

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/HomePage'
-import StoryPage from './pages/StoryPage'
-import MenuPage from './pages/MenuPage'
-import ShopPage from './pages/ShopPage'
-import ContactPage from './pages/ContactPage'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/HomePage';
+import StoryPage from './pages/StoryPage';
+import MenuPage from './pages/MenuPage';
+import ShopPage from './pages/ShopPage';
+import ContactPage from './pages/ContactPage';
 
 export default function App() {
   return (
-    <>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/story" element={<StoryPage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/menu"  element={<MenuPage />} />
+        <Route path="/shop"  element={<ShopPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
       <Footer />
-    </>
-  )
+    </HashRouter>
+  );
 }
